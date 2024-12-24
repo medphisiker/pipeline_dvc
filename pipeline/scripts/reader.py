ee1ef991db565271d0fec6447fbcd93510dc7712
Anton Shiryaev <a.shiryaev@cashalot.mcfef.ru>
2024-12-25 03:17:28 +1000

    feat(model bench simulation): Переделать пример на симуляцию бенчмарка
    Переделать пример на симуляцию бенчмарка

--- pipeline/scripts/reader.py
+++ pipeline/scripts/reader.py
@@ -1,6 +1,5 @@
 import os
 import time
-import torch
 
 
 if __name__ == "__main__":
@@ -9,11 +8,6 @@ if __name__ == "__main__":
     output_txt = "data/task2_output.txt"
 
     os.system("nvidia-smi")
-    print("torch.__version__", torch.__version__)
-    print("torch.cuda.is_available()", torch.cuda.is_available())
-    print("torch.cuda.current_device()", torch.cuda.current_device())
-    print("torch.cuda.device_count()", torch.cuda.current_device())
-    print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
 
     with open(input_txt, "r") as input_file, open(output_txt, "w") as output_file:
         # Читаем файл построчно
--- pipeline/scripts/writer.py
+++ pipeline/scripts/writer.py
@@ -1,5 +1,6 @@
 import os
 import time
+import torch
 
 
 if __name__ == "__main__":
@@ -7,9 +8,14 @@ if __name__ == "__main__":
     filename = "data/task1_output.txt"
 
     os.system("nvidia-smi")
+    print("torch.__version__", torch.__version__)
+    print("torch.cuda.is_available()", torch.cuda.is_available())
+    print("torch.cuda.current_device()", torch.cuda.current_device())
+    print("torch.cuda.device_count()", torch.cuda.current_device())
+    print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
 
     with open(filename, "w") as file:
-        for i in range(10):
+        for i in range(12):
             line = f"{i}\n"
             file.write(line)
             print(line)
